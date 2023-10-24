60f0dbccbb03c2c02c4be32223ea90a92500f88e
mi-hwa <43jmih@naver.com>
2023-10-25 08:38:07 +0900

    영속성 관리 - 내부 동작 방식 : 준영속성 상태

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,13 +15,11 @@ public class JpaMain {
     tx.begin();
 
     try {
-      Member member = new Member(200L, "member200");
-      em.persist(member);
+      Member member = em.find(Member.class, 150L);
+      member.setName("AAAAA");
 
-      em.flush();
-      //--> flush를 해도 영속성 컨텍스트(1차 캐시)는 유지되며,
-      // 영속성 컨텍스트의 변경내용(쓰기 지연 SQL 저장소에 있는 SQL)을 DB에 동기화한다.
-      //--> 트랜잭션이라는 작업 단위가 중요! 커밋 직전에만 동기화하면 됨!
+      em.detach(member);
+      //--> 준영속 상태가 되어 변경사항에서 빠짐
 
       System.out.println("=================================");
       tx.commit();
--- src/main/java/hellojpa/PersistenceContext.java
+++ src/main/java/hellojpa/PersistenceContext.java
@@ -102,4 +102,31 @@ public class PersistenceContext {
 
     emf.close();
   }
+
+  void persistence4() {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+    EntityManager em = emf.createEntityManager();
+
+    EntityTransaction tx = em.getTransaction();
+    tx.begin();
+
+    try {
+      Member member = new Member(200L, "member200");
+      em.persist(member);
+
+      em.flush();
+      //--> flush를 해도 영속성 컨텍스트(1차 캐시)는 유지되며,
+      // 영속성 컨텍스트의 변경내용(쓰기 지연 SQL 저장소에 있는 SQL)을 DB에 동기화한다.
+      //--> 트랜잭션이라는 작업 단위가 중요! 커밋 직전에만 동기화하면 됨!
+
+      System.out.println("=================================");
+      tx.commit();
+    } catch (Exception e) {
+      tx.rollback();
+    } finally {
+      em.close();
+    }
+
+    emf.close();
+  }
 }
