b71ca8e819dae308ad464c9250a5f2124f991839
mi-hwa <43jmih@naver.com>
2023-10-26 08:31:56 +0900

    엔티티 매핑 : 기본 키 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,22 +1,32 @@
 package hellojpa;
 
-import java.time.LocalDate;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
-import javax.persistence.Table;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
 @Entity
+@SequenceGenerator(
+    name = "MEMBER_SEQ_GENERATOR",
+    sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+    initialValue = 1, allocationSize = 1
+)
 public class Member {
 
   @Id
+  @GeneratedValue(
+      strategy = GenerationType.SEQUENCE,
+      generator = "MEMBER_SEQ_GENERATOR"
+  )
   private Long id;
   @Column(name = "name")
   private String username;
