064684f2abced309d605bf15220862367a674d11
mi-hwa <43jmih@naver.com>
2023-10-27 09:06:23 +0900

    양방향 연관관계와 연관관계의 주인 1- 기본

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -25,9 +26,18 @@ public class JpaMain {
       member.setTeam(team);
       em.persist(member);
 
+      em.flush();
+      em.clear();
+
       // 조회
       Member findMember = em.find(Member.class, member.getId());
-      System.out.println("findMember.Team = " + findMember.getTeam());
+      System.out.println("findMember.Team = " + findMember.getTeam().getName());
+
+      // 양방향 연관관계
+      List<Member> members = findMember.getTeam().getMembers();
+      for (Member m : members) {
+        System.out.println("m.name = " + m.getName());
+      }
 
       tx.commit();
     } catch (Exception e) {
