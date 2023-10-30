1084a5da4509ffa3d89f7c9d97c7d2bdfb1cc9ac
mi-hwa <43jmih@naver.com>
2023-10-30 10:10:17 +0900

    리팩토링: JpaMain.java -> EntityMapping.java 분리 및 주석 처리

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,5 @@
 package hellojpa;
 
-import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -16,34 +15,6 @@ public class JpaMain {
     tx.begin();
 
     try {
-      // 저장
-      Team team = new Team();
-      team.setName("TeamA");
-      //team.getMembers().add(member); //연관관계의 주인이 아니기 때문에 읽기 전용
-      em.persist(team);
-
-      Member member = new Member();
-      member.setName("member1");
-      //member.changeTeam(team); //**
-      em.persist(member);
-
-      team.addMember(member);
-      //team.getMembers().add(member); //**
-
-      //em.flush();
-      //em.clear();
-
-      // 조회
-      Member findMember = em.find(Member.class, member.getId()); //1차 캐시
-      System.out.println("findMember.Team = " + findMember.getTeam().getName());
-
-      // 양방향 연관관계
-      List<Member> members = findMember.getTeam().getMembers();
-      System.out.println("=================================");
-      for (Member m : members) {
-        System.out.println("m.name = " + m.getName());
-      }
-      System.out.println("=================================");
 
       tx.commit();
     } catch (Exception e) {
--- src/main/java/hellojpa/practice/EntityMapping.java
+++ src/main/java/hellojpa/practice/EntityMapping.java
@@ -1,38 +1,81 @@
-//package hellojpa.practice;
-//
-//import hellojpa.Member;
-//import javax.persistence.EntityManager;
-//import javax.persistence.EntityManagerFactory;
-//import javax.persistence.EntityTransaction;
-//import javax.persistence.Persistence;
-//
-//public class EntityMapping {
-//
-//  public void identity() {
-//    EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-//    EntityManager em = emf.createEntityManager();
-//
-//    EntityTransaction tx = em.getTransaction();
-//    tx.begin();
-//
-//    try {
-//      Member member = new Member();
-//      member.setUsername("C");
-//
-//      System.out.println("=================================");
-//      em.persist(member);
-//      System.out.println("member.Id = " + member.getId());
-//      System.out.println("=================================");
-//      // GenerationType.IDENTITY
-//      // -> DB에 insert 해야만 ID 를 알 수 있기 때문에, persist 시점에서 insert 한다. (다른 경우 commit 해야 insert 하는 것과 대조적)
-//
-//      tx.commit();
-//    } catch (Exception e) {
-//      tx.rollback();
-//    } finally {
-//      em.close();
-//    }
-//
-//    emf.close();
-//  }
-//}
+/*
+package hellojpa.practice;
+
+import hellojpa.Member;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class EntityMapping {
+
+  public void identity() {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+    EntityManager em = emf.createEntityManager();
+
+    EntityTransaction tx = em.getTransaction();
+    tx.begin();
+
+    try {
+      Member member = new Member();
+      member.setUsername("C");
+
+      System.out.println("=================================");
+      em.persist(member);
+      System.out.println("member.Id = " + member.getId());
+      System.out.println("=================================");
+      // GenerationType.IDENTITY
+      // -> DB에 insert 해야만 ID 를 알 수 있기 때문에, persist 시점에서 insert 한다. (다른 경우 commit 해야 insert 하는 것과 대조적)
+
+      tx.commit();
+    } catch (Exception e) {
+      tx.rollback();
+    } finally {
+      em.close();
+    }
+
+    emf.close();
+  }
+
+  public void mapping() {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+    EntityManager em = emf.createEntityManager();
+
+    EntityTransaction tx = em.getTransaction();
+    tx.begin();
+
+    try {
+      // 저장
+      Team team = new Team();
+      team.setName("TeamA");
+      em.persist(team);
+
+      Member member = new Member();
+      member.setName("member1");
+      em.persist(member);
+
+      team.addMember(member);
+
+      // 조회
+      Member findMember = em.find(Member.class, member.getId()); //1차 캐시
+      System.out.println("findMember.Team = " + findMember.getTeam().getName());
+
+      // 양방향 연관관계
+      List<Member> members = findMember.getTeam().getMembers();
+      System.out.println("=================================");
+      for (Member m : members) {
+        System.out.println("m.name = " + m.getName());
+      }
+      System.out.println("=================================");
+
+      tx.commit();
+    } catch (Exception e) {
+      tx.rollback();
+    } finally {
+      em.close();
+    }
+
+    emf.close();
+  }
+}
+*/
