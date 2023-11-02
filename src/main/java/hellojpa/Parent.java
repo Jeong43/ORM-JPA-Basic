c4afc03c20ea04c1abf7faaa2304abd3069b1ed5
mi-hwa <43jmih@naver.com>
2023-11-02 10:40:54 +0900

    프록시와 연관관계 관리: 고아 객체

--- src/main/java/hellojpa/Parent.java
+++ src/main/java/hellojpa/Parent.java
@@ -17,7 +17,12 @@ public class Parent {
   @GeneratedValue
   private Long id;
   private String name;
-  @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL) //하나의 부모가 자식들을 관리할 때 의미가 있음
+  @OneToMany(
+      mappedBy = "parent",
+      cascade = CascadeType.ALL, //하나의 부모가 자식들을 관리할 때 의미가 있음
+      orphanRemoval = true //부모 엔티티와 연관관계가 끊긴 자식 엔티티는 자동으로 삭제
+      //==> 참조하는 곳이 하나일 때 사용해야 함(특정 엔티티가 개인 소유할 때)
+  )
   private List<Child> childList = new ArrayList<>();
 
   public void addChild(Child child) {
